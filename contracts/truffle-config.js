19e571f0c30d3e7fe4d36895eda4ff08021102b1
Sri Krishna Chaithanya Raj Masimukku <mskchaithanyaraj@gmail.com>
2025-10-04 09:33:02 +0530

    Created project structure and Setup Contracts Project with Truffle

---
+++ contracts/truffle-config.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+module.exports = {
+  networks: {
+    // Development network for Ganache
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777", // Match Ganache network ID
+      gas: 6721975,
+      gasPrice: 20000000000, // 20 gwei
+      from: undefined, // Will use the first account from provider
+      provider: function () {
+        return new HDWalletProvider({
+          privateKeys: [process.env.DEPLOYER_PRIVATE_KEY],
+          providerOrUrl: process.env.ETH_RPC_URL || "http://127.0.0.1:7545",
+          numberOfAddresses: 1,
+          shareNonce: true,
+          derivationPath: "m/44'/60'/0'/0/",
+        });
+      },
+    },
+
+    // Development network (Truffle's built-in)
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+  },
+
+  // Configure compilers
+  compilers: {
+    solc: {
+      version: "0.8.20",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+
+  // Directory structure
+  contracts_directory: "./contracts",
+  contracts_build_directory: "./build/contracts",
+  migrations_directory: "./migrations",
+  test_directory: "./test",
+
+  // Plugins
+  plugins: [],
+};
